d167900c4552f5a2324d17ed4a49fd4098501e3a
Mitalee Desai <mitaleedesai@Mitalees-MacBook-Pro-2.local>
2024-12-07 23:35:16 -0800

    Add missing files from initial commit

---
+++ init.lua
@@ -0,0 +1,15 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+vim.api.nvim_set_option("clipboard", "unnamed")
+vim.opt.relativenumber = true
+
+-- Navigate vim panes better
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+
+require("config.lazy")
