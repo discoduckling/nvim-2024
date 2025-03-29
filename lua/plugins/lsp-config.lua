d732b0a7af47409405e475eab53a643caf12e635
Mitalee Desai <mitaleedesai@Mitalees-MacBook-Pro-2.local>
2025-03-28 22:51:51 -0700

    Update nvim config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -25,6 +25,7 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			capabilities.textDocument.completion.completionItem.snippetSupport = true
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
@@ -32,9 +33,34 @@ return {
 			lspconfig.ts_ls.setup({
 				capabilities = capabilities,
 			})
-      lspconfig.gopls.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.emmet_ls.setup({
+				capabilities = capabilities,
+				filetypes = {
+					"css",
+					"eruby",
+					"html",
+					"javascript",
+					"javascriptreact",
+					"less",
+					"sass",
+					"scss",
+					"svelte",
+					"pug",
+					"typescriptreact",
+					"vue",
+				},
+				init_options = {
+					html = {
+						options = {
+							-- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+							["bem.enabled"] = true,
+						},
+					},
+				},
+			})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
