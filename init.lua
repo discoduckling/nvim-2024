e3e73ffaa765f07c391e13e0ec5db4c6005ecbfb
Mitalee Desai <mitaleedesai@Mitalees-MacBook-Pro-2.local>
2025-03-29 23:48:47 -0700

    Clean up scroll experience

--- init.lua
+++ init.lua
@@ -6,6 +6,13 @@ vim.cmd("set number")
 vim.api.nvim_set_option("clipboard", "unnamed")
 vim.opt.relativenumber = true
 
+-- Improve scrolling experience
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.scroll = 20 
+vim.keymap.set("n", "<C-d>", "<C-d>zz", {desc = "Center cursor after moving down half-page"})
+vim.keymap.set("n", "<C-u>", "<C-u>zz", {desc = "Center cursor after moving up half-page"})
+
 -- Navigate vim panes better
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
